d91b3726dc16766646621eb0e2e47fb61aece900
Vitor Andrews <vitorandrewssvitorino@gmail.com>
2023-03-05 14:07:30 -0300

    First commit
    First commit for create archives on repository

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Here it all begins.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
